3e7e9291dbda57ca0ed3e5b34629e727523ad50d
Hunter Antal <hunterantalgithub@gmail.com>
2023-11-17 12:58:39 -0500

    Initial Commit

---
+++ Assignment 3 Heap Sort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Assignment 3 Heap Sort
+//
+//  Created by Hunter Antal on 2023-11-17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
